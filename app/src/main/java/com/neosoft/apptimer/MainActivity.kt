617e5fbf0a5dcfb6f3afddc7d6f6201036000618
viraj pote <virajpote99@gmail.com>
2022-09-13 22:08:39 +0530

    commit

--- app/src/main/java/com/neosoft/apptimer/MainActivity.kt
+++ app/src/main/java/com/neosoft/apptimer/MainActivity.kt
@@ -1,7 +1,6 @@
 package com.neosoft.apptimer
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_main.*
 
@@ -9,8 +8,7 @@ class MainActivity : BaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        txt_main.setOnClickListener{
+        btn_main.setOnClickListener {
             val intent = Intent(this,HomeActivity::class.java)
             startActivity(intent)
         }
